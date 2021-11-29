e8042c17de373c2ef958a4ff70a9d0ee8a75cbad
Enrique Castrillo <correo_enrique@hotmail.com>
2021-11-29 13:04:22 +0100

    Primera subida. App funcionando según peticiones de slides.

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  public latitudPromesa: number;
+  public longitudPromesa: number;
+  public latitudObservable: number;
+  public longitudObservable: number;
+
+  constructor(private geolocation: Geolocation) {
+
+    this.latitudPromesa = 0;
+    this.longitudPromesa = 0;
+
+    this.latitudObservable = 0;
+    this.longitudObservable = 0;
+
+  }
+
+  getLocProm(): void{
+
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.latitudPromesa = resp.coords.latitude;
+      this.longitudPromesa = resp.coords.longitude;
+    }).catch((error)=>
+      console.error('Error al obtener la geolocalización' + error)
+    );
+  }
+
+  getLocObs(): void{
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data: Geoposition) => {
+      this.latitudObservable = data.coords.latitude;
+      this.longitudPromesa = data.coords.longitude;
+    });
+  }
+}
